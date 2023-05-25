63ab69296f158c6dd4cf7dc1f118f9fc6f2c66a6
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2023-05-25 19:13:38 +0200

    fixes and cleanup

--- deploy-azure-function.sh
+++ deploy-azure-function.sh
@@ -16,5 +16,4 @@ DEPLOYMENT_USER='$func-dotnet-deployment-test'
 DEPLOYMENT_PASSWORD='***********************'
 CREDENTIALS=$DEPLOYMENT_USER:$DEPLOYMENT_PASSWORD
 curl -v -X POST --user $CREDENTIALS --data-binary @"publish.zip" https://$DEPLOYMENT_APP.scm.azurewebsites.net:443/api/zipdeploy
-#* TLSv1.2 (IN), TLS handshake, Certificate (11):
 curl -X GET --user $CREDENTIALS https://$DEPLOYMENT_APP.scm.azurewebsites.net:443/deployments
